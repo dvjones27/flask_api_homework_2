0cca982223d7f3a185483450ff3b479dcd17f141
dvjones27 <102387309+dvjones27@users.noreply.github.com>
2022-11-27 15:27:15 -0500

    Flask API Homework 2: Create a User Form and User Database For Car Collection API First Commit

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from  dotenv import  load_dotenv
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config():
+    '''
+        Set config variables for the flask app using Environment variables where available. Otherwise, create the config if not done already.
+    '''
+    
+    FLASK_APP = os.getenv('FLASK_APP')
+    FLASK_ENV = os.getenv('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'This is something that I should remember'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_NOTIFICATIONS = False
